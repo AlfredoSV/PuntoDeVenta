236b34bdf80aacde8fc2e085def228dc3baaa7b9
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-08 19:14:22 -0600

    Se agrega metódo ConsultarProductos en RepositorioCatalogos

--- AccesoDatos/Repositorios/RepositorioCatalogos.cs
+++ AccesoDatos/Repositorios/RepositorioCatalogos.cs
@@ -1,34 +1,70 @@
-﻿using System;
+﻿using Dominio.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AccesoDatos.Repositorios
 {
     public  class RepositorioCatalogos
     {
-        Guid _idProveedor;
-        string _nombre;
-        string _descripcion;
-        DateTime _fechayHoraAlta;
+        private static RepositorioCatalogos _instacia;
+        private static string _cadCon;
 
-        public Guid IdProveedor { get => _idProveedor; set => _idProveedor = value; }
-        public string Nombre { get => _nombre; set => _nombre = value; }
-        public string Descripcion { get => _descripcion; set => _descripcion = value; }
-        public DateTime FechayHoraAlta { get => _fechayHoraAlta; set => _fechayHoraAlta = value; }
-
-        public RepositorioCatalogos(Guid idProveedor, string nombre, string descripcion, DateTime fechayHoraAlta)
+        public static RepositorioCatalogos Instacia
         {
-            IdProveedor = idProveedor;
-            Nombre = nombre;
-            Descripcion = descripcion;
-            FechayHoraAlta = fechayHoraAlta;
+            get
+            {
+                if (_instacia == null)
+                    _instacia = new RepositorioCatalogos();
+
+                return _instacia;
+
+            }
+            set => _instacia = value;
         }
 
-        public static RepositorioCatalogos Create (Guid idProveedor, string nombre, string descripcion, DateTime fechayHoraAlt)
+        private RepositorioCatalogos() { }
+
+
+        public async Task<IEnumerable<CatProveedores>> ConsultarProductos(string buscar)
         {
+            var provedores = new List<CatProveedores>();
+            var sql = "select * from Proveedores;";
+            SqlDataReader sqlDataReader;
+            SqlCommand sqlCommand;
 
-            return new RepositorioCatalogos( idProveedor,  nombre,  descripcion,  fechayHoraAlt);
+            try
+            {
+                using (var conexion = new SqlConnection(_cadCon))
+                {
+                    conexion.Open();
 
+                    sqlCommand = new SqlCommand(sql, conexion);
+        
+                    sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            provedores.Add(CatProveedores.Crear(sqlDataReader.GetGuid(0), sqlDataReader.GetString(1),
+                                sqlDataReader.GetString(2), sqlDataReader.GetDateTime(3)));
+                        }
+                    }
+
+
+                    return provedores;
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
         }
+
+
     }
 }
--- Dominio/Entidades/CatProveedores.cs
+++ Dominio/Entidades/CatProveedores.cs
@@ -6,5 +6,29 @@ namespace Dominio.Entidades
 {
     public  class CatProveedores
     {
+        Guid _idProveedor;
+        string _nombre;
+        string _descripcion;
+        DateTime _fechayHoraAlta;
+
+        public Guid IdProveedor { get => _idProveedor; set => _idProveedor = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public DateTime FechayHoraAlta { get => _fechayHoraAlta; set => _fechayHoraAlta = value; }
+
+        public CatProveedores(Guid idProveedor, string nombre, string descripcion, DateTime fechayHoraAlta)
+        {
+            IdProveedor = idProveedor;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            FechayHoraAlta = fechayHoraAlta;
+        }
+
+        public static CatProveedores Crear(Guid idProveedor, string nombre, string descripcion, DateTime fechayHoraAlt)
+        {
+
+            return new CatProveedores(idProveedor, nombre, descripcion, fechayHoraAlt);
+
+        }
     }
 }
