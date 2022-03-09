74e1e6d6f7f367aa8239df675010e158e72d1567
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-08 18:58:14 -0600

    Se agrega ServicioCatalogos

---
+++ Negocio/Servicios/ServicioCatalogos.cs
@@ -0,0 +1,36 @@
+﻿using Aplicacion.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Servicios
+{
+    public  class ServicioCatalogos
+    {
+        private static ServicioCatalogos _instacia;
+     
+
+        public static ServicioCatalogos Instacia
+        {
+            get
+            {
+                if (_instacia == null)
+                    _instacia = new ServicioCatalogos();
+
+                return _instacia;
+
+            }
+            set => _instacia = value;
+        }
+
+
+        public ServicioCatalogos() { }
+
+
+        public async Task<CatProveedores> ConsultarProveedoresBD()
+        {
+            return null;
+        }
+    }
+}
