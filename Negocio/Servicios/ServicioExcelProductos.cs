7a611765ae201926c4cb84cd401dcc2c6eaba48b
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-15 18:02:07 -0600

    Se agrega ServicioExcelProductos

---
+++ Negocio/Servicios/ServicioExcelProductos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Dominio.Entidades;
+using SpreadsheetLight;
+using SpreadsheetLight.Drawing;
+
+namespace Aplicacion.Servicios
+{
+    public class ServicioExcelProductos
+    {
+        public IEnumerable<Producto> LeerExcel(MemoryStream archivo)
+        {
+            var listaElmen = new List<Producto>();
+            using (var sp = new SLDocument(archivo))
+            {
+                var wsn = sp.GetWorksheetStatistics();
+
+                for (int i = 1; i <= wsn.EndRowIndex; i++)
+                {
+                }
+
+            }
+
+            return listaElmen;
+
+        }
+    }
+}
