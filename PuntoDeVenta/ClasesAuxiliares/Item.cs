cc43a7d63c7ff501a3ba848b9a17f63f7d63ef27
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-18 13:46:35 -0600

    Se agrega clase CargarCombo con posible solución para cargar valores a comboBox de forma genérica

--- PuntoDeVenta/ClasesAuxiliares/Item.cs
+++ PuntoDeVenta/ClasesAuxiliares/Item.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace PuntoDeVenta.ClasesAuxiliares
+namespace PuntoDeVenta.ClasesAuxiliares 
 {
 
     public class Item
@@ -14,6 +16,31 @@ namespace PuntoDeVenta.ClasesAuxiliares
         public Item(string _name, Guid _value)
         {
             Name = _name; Value = _value;
+        }
+
+       
+    }
+
+    public class CargarCombo
+    {
+        private void CargarComboItems(IEnumerable<object> datos, string mensajeDefault, ComboBox comboOb,string campoNombre, string campoId)
+        {
+            var items = new List<Item>();
+
+            items.Add(new Item($"--- {mensajeDefault}. ---", Guid.Empty));
+
+            Type t = datos.FirstOrDefault().GetType();
+
+            PropertyInfo[] propiedades = t.GetProperties();
+
+            foreach (var get in propiedades)
+            {
+                items.Add(new Item( get.GetValue(datos).ToString(), Guid.Parse(get.GetValue(datos).ToString())));
+            }
+
+            comboOb.DisplayMember = "Name";
+            comboOb.ValueMember = "Value";
+            comboOb.DataSource = items;
         }
     }
 }
