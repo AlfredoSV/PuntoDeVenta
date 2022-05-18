bd2743ce68546a8d22757521f3e0425dffaa95e6
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-05-17 20:41:06 -0500

    Se agrega DtoCompraProductos y DtoProductoSeleccionado

---
+++ Negocio/Dtos/DtoCompraProductos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.Dtos
+{
+    public class DtoCompraProductos
+    {
+        public Guid IdCompra { get; set; }
+        public DtoProductoSeleccionado Productos {get; set;}
+        public decimal Total { get; set; }
+        public int TotalProductos { get; set; }
+    }
+}
---
+++ Negocio/Dtos/DtoProductoSeleccionado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.Dtos
+{
+    public class DtoProductoSeleccionado
+    {
+        public Guid IdProducto { get; set; }
+        public int Cantidad { get; set; }
+
+        public int Subtotal { get; set; }
+    }
+}
